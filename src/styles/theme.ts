e1af29fdf32c0ee0c39f4a99e824e97044625364
Ben <ben.pmt2d@gmail.com>
2020-12-12 12:52:03 +0800

    theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,15 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core';
+import { blueGrey, grey } from '@material-ui/core/colors';
 
-export const defaultTheme = responsiveFontSizes(createMuiTheme());
+export const defaultTheme = responsiveFontSizes(
+    createMuiTheme({
+        palette: {
+            primary: {
+                main: blueGrey['800']
+            },
+            secondary: {
+                main: grey['800']
+            },
+        }
+    })
+);
