c6c7b08eedb9654d00eb4d65cc728499f2b25de9
Ben <ben.pmt2d@gmail.com>
2020-12-13 01:09:37 +0800

    separate data fetcher

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,12 @@
-export interface IXmlEquipment {
-    '@_type': string;
-    '@_id': string;
-    Class: 'Equipment';
-    Tier: number;
-    SlotType: EEquipmentSlot;
+export type IStocks = Array<{ id: number, quantity: number }>;
+
+export type IOffer = {
+    quantity: number,
+    sellingItems: number[],
+    sellingQuantities: number[],
+    buyingItems: number[],
+    buyingQuantities: number[],
+    suspended: boolean,
 }
 
 export enum EWeaponSlot {
